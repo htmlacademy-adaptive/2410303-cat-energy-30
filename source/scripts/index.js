dd5a96ead9949262ca7b2063fb005689cf6098d7
YaroslavNE-Frontend <yaroslav.nov2001@gmail.com>
2023-12-08 10:00:51 +0900

    Добавляет форматирование js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,51 +1,48 @@
 /* в этот файл добавляет скрипты*/
 
-// Подстановка нужной ссылки по клику
-
-
 const shopData = {
-    shop1: [{
-        lat: 55.75222,
-        lon: 37.61556,
-        name: 'Кэт Энерджи',
-        iconDefault: 'default#image',
-        iconHref: '../raw/images/cat.png',
-        iconSize: [113, 106],
-        iconOffset: [-5, -38]
-    }],
+  shop1: [{
+    lat: 55.75222,
+    lon: 37.61556,
+    name: 'Кэт Энерджи',
+    iconDefault: 'default#image',
+    iconHref: '../raw/images/cat.png',
+    iconSize: [113, 106],
+    iconOffset: [-5, -38]
+  }],
 };
 
 const init = () => {
-    const map = new ymaps.Map('map', {
-        center: [55.753215, 37.622504],
-        zoom: 14
+  const map = new ymaps.Map('map', {
+    center: [55.753215, 37.622504],
+    zoom: 14
+  });
+
+  let activeShop = "shop1";
+
+  const showShop = (shop) => {
+    map.geoObjects.removeAll();
+
+    shopData[shop].forEach((item) => {
+      const placemark = new ymaps.Placemark([item.lat, item.lon], {
+        hintContent: item.name,
+        balloonContent: item.name,
+      }, {
+        iconLayout: item.iconDefault,
+        iconImageHref: item.iconHref,
+        iconImageSize: item.iconSize,
+        iconImageOffset: item.iconOffset
+      });
+
+      map.geoObjects.add(placemark);
+
+
     });
 
-    let activeShop = "shop1";
+    activeShop = shop;
+  }
 
-    const showShop = (shop) => {
-        map.geoObjects.removeAll();
-
-        shopData[shop].forEach((item) => {
-            const placemark = new ymaps.Placemark([item.lat, item.lon], {
-                hintContent: item.name,
-                balloonContent: item.name,
-            }, {
-                iconLayout: item.iconDefault,
-                iconImageHref: item.iconHref,
-                iconImageSize: item.iconSize,
-                iconImageOffset:item.iconOffset
-            });
-
-            map.geoObjects.add(placemark);
-
-
-        });
-
-        activeShop = shop;
-    }
-
-    showShop(activeShop);
+  showShop(activeShop);
 };
 
 ymaps.ready(init);
