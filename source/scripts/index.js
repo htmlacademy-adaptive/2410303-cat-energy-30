de80fbd7577402bb28b49ac15d3446f0b2d4e3db
YaroslavNE-Frontend <yaroslav.nov2001@gmail.com>
2023-12-21 20:40:41 +0900

    Добавляет мобильный header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,36 @@
 /* в этот файл добавляет скрипты*/
+
+let BurgerHeader = document.querySelector(".page-header__toggle");
+
+let HeaderMenu =  document.querySelector(".suite-list");
+
+if (BurgerHeader) {
+  BurgerHeader.addEventListener("click", function() {
+    BurgerHeader.classList.toggle("page-header__toggle--close");
+    BurgerHeader.classList.toggle("page-header__toggle--open");
+    HeaderMenu.classList.toggle("open");
+  })
+}
+
+// if (navToggle) {
+//   navToggle.classList.remove("main-nav__toggle--hidden");
+//   mainNav.classList.add("main-nav--closed");
+//   navToggle.addEventListener("click", function() {
+//       mainNav.classList.toggle("main-nav--opened");
+//       mainNav.classList.toggle("main-nav--closed");
+//   })
+// }
+
+
+
+
+
+
+// else {
+//     BurgerHeader.classList.remove("page-header__toggle--open");
+//     BurgerHeader.classList.toggle("page-header__toggle--close");
+
+// };
+
+
+
